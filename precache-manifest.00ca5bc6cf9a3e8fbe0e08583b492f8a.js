ccfb9d7574c47fd7cd650077c462e097403961ed
Ron <laniado.ron@gmail.com>
2021-02-14 18:46:03 -0500

    Updates

---
+++ precache-manifest.00ca5bc6cf9a3e8fbe0e08583b492f8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdbc3b9df634e637090a0974d1419093",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d724a0caf5af836f458",
+    "url": "/static/css/main.9ab1eee2.chunk.css"
+  },
+  {
+    "revision": "fb728397fde009a5e1ed",
+    "url": "/static/js/2.2ed1661d.chunk.js"
+  },
+  {
+    "revision": "1d724a0caf5af836f458",
+    "url": "/static/js/main.7dcf9c91.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
