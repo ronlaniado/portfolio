1d285c521cc4fea24626bb01fb857570742608ac
Ron <laniado.ron@gmail.com>
2021-02-14 17:57:51 -0500

    Updates

---
+++ precache-manifest.37ac336bc92b1e159e4cc980a223862c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fb779426ffb3494f250ede9654de569",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "835cba6e05fa31817580",
+    "url": "/portfolio/static/css/main.9ab1eee2.chunk.css"
+  },
+  {
+    "revision": "fb728397fde009a5e1ed",
+    "url": "/portfolio/static/js/2.2ed1661d.chunk.js"
+  },
+  {
+    "revision": "835cba6e05fa31817580",
+    "url": "/portfolio/static/js/main.9b620180.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  }
+]);
