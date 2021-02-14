2b9659f3d249685f70863cf90bc72d1ac25fbddc
Ron <laniado.ron@gmail.com>
2021-02-14 17:57:59 -0500

    publish attempt 2

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "ronlaniado.me/precache-manifest.28b79bb672938e4ab76b97878d60bfe2.js"
+  "/portfolio/precache-manifest.37ac336bc92b1e159e4cc980a223862c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("ronlaniado.me/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
